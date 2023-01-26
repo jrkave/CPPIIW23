28596b913faee72fdc68231572d59b3343ce5907
jrkave <110064455+jrkave@users.noreply.github.com>
2023-01-26 09:17:51 +0000

    Second commit

--- header.h
+++ header.h
@@ -12,7 +12,48 @@
 using namespace std;
 
 // Declare variables
+    int myIndex = 0;
+    string userInput;
 
-// Declare functions, classes
+// Creation of vectors for ISBN, Title, Author, Publication, Publisher
+    rapidcsv::Document doc("books.csv");
+    vector<string> vISBN = doc.GetColumn<string>("ISBN");
+    vector<string> vTitle = doc.GetColumn<string>("Book-Title");
+    vector<string> vAuthor = doc.GetColumn<string>("Book-Author");
+    vector<string> vYear = doc.GetColumn<string>("Year-Of-Publication");
+    //vector<string> vPublisher = doc.GetColumn<string>("Publisher");
+
+// Function to find appropriate index of vectors
+    void BookInformation(string key) {
+        // Finds index if key is an ISBN 
+        if (isdigit(key[0])) {
+            vector<string>::iterator itr = find(vISBN.begin(), vISBN.end(), key);
+            if (itr != vISBN.cend()) {
+                myIndex = distance(vISBN.begin(), itr);
+                cout << "ISBN Number: " << vISBN[myIndex] << endl;
+                cout << "Book Title: " << vTitle[myIndex] << endl;
+                cout << "Book Author: " << vAuthor[myIndex] << endl;
+                cout << "Year of Publication: " << vYear[myIndex] << endl; 
+                //cout << "Publisher " << vPublisher[myIndex] << endl;
+            }
+            else {
+                cout << "Book not found. " << endl;
+            }
+        // Finds index if key is a title
+        } else {
+            vector<string>::iterator itr2 = find(vTitle.begin(), vTitle.end(), key);
+            if (itr2 != vTitle.cend()) {
+                myIndex = distance(vTitle.begin(), itr2);
+                cout << "ISBN Number: " << vISBN[myIndex] << endl;
+                cout << "Book Title: " << vTitle[myIndex] << endl;
+                cout << "Book Author: " << vAuthor[myIndex] << endl;
+                cout << "Year of Publication: " << vYear[myIndex] << endl; 
+                //cout << "Publisher: " << vPublisher[myIndex] << endl;
+            }
+            else {
+                cout << "Book not found. " << endl;
+            }
+        }
+    }
 
 #endif /* HEADER_H_ */
--- main.cpp
+++ main.cpp
@@ -4,7 +4,26 @@
 *   Author(s): Veenkamp, Brooks, James
 */
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include "header.h"
+using namespace std;
 
 int main() {
+
+// Example 1
+    userInput = "Stardust";
+    BookInformation(userInput);
+    cout << endl;
+
+// Example 2
+    userInput = "0425061957";
+    BookInformation(userInput);
+
+// Prompts user for book title or ISBN number
+    cout << "To view information about a book, please enter its title or ISBN: " << endl;
+    cin >> userInput;
+    BookInformation(userInput);
 
 }
