b7eca23856eeb5576064e936b17f9bcc932c5b5d
jrkave <110064455+jrkave@users.noreply.github.com>
2023-01-26 06:59:38 +0000

    First commit

---
+++ header.h
@@ -0,0 +1,18 @@
+/* 
+*   Filename: header.h
+*   Date: Jan. 26, 2023
+*   Author(s): Veenkamp, Brooks, James
+*/
+
+#include <iostream>
+#include "rapidcsv.h"
+
+#ifndef HEADER_H_
+#define HEADER_H_
+using namespace std;
+
+// Declare variables
+
+// Declare functions, classes
+
+#endif /* HEADER_H_ */
---
+++ main.cpp
@@ -0,0 +1,10 @@
+/* 
+*   Filename: main.cpp
+*   Date: Jan. 26, 2023
+*   Author(s): Veenkamp, Brooks, James
+*/
+
+
+int main() {
+
+}
