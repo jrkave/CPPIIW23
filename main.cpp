480d213f2179e33e5971bfa1967d6486fc15c405
jrkave <110064455+jrkave@users.noreply.github.com>
2023-02-01 05:47:55 +0000

    Added user input

--- main.cpp
+++ main.cpp
@@ -10,12 +10,35 @@ using namespace std;
 
 int main() {
 
-// Example 1
-    BookInformation book1("The Giver");
+    // Welcome message
+    cout << "Welcome to our book information program! Shortly, you will be prompted to input a book's title or ISBN." << endl;
+    cout << "If your book is in our system, we will return the following information: " << endl;
+    cout << "ISBN, Title, Author, Year of Publication, Publisher " << endl;
+    cout << endl;
+
+
+    // User input
+    string input;
+    cout << "Please enter a book's title or ISBN number: " << endl;
+    cin >> input;
+
+    // Create instance of class BookInformation using input
+    BookInformation book1(input);
     book1.Display();
 
-// Example 2
-    BookInformation book2("Introducing Nietzsche (Foundations in Children's Ministry)");
-    book2.Display();
-
+    // Continue prompt
+    char quitProgram = ' ';
+    while (quitProgram != 'q') {
+        cout << "Would you like to quit? Enter q for quit, and any other key to continue. " << endl;
+        cin >> quitProgram;
+        if (quitProgram == 'q') {
+            break;
+        }
+        else {
+            cout << "Please enter a book's title or ISBN number: " << endl;
+            cin >> input;
+            BookInformation book1(input);
+            book1.Display();
+        }
+    }
 };
