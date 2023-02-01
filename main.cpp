9e927ef550d2eddfa599ea175c389833bf4df989
jrkave <110064455+jrkave@users.noreply.github.com>
2023-02-01 06:03:18 +0000

    Minor edit

--- main.cpp
+++ main.cpp
@@ -13,14 +13,15 @@ int main() {
     // Welcome message
     cout << "Welcome to our book information program! Shortly, you will be prompted to input a book's title or ISBN." << endl;
     cout << "If your book is in our system, we will return the following information: " << endl;
-    cout << "ISBN, Title, Author, Year of Publication, Publisher " << endl;
+    cout << "ISBN, Title, Author, Year of Publication, and Publisher. " << endl;
     cout << endl;
-
 
     // User input
     string input;
     cout << "Please enter a book's title or ISBN number: " << endl;
     cin >> input;
+    cout << "Retrieving book information..." << endl;
+    cout << endl;
 
     // Create instance of class BookInformation using input
     BookInformation book1(input);
@@ -29,7 +30,7 @@ int main() {
     // Continue prompt
     char quitProgram = ' ';
     while (quitProgram != 'q') {
-        cout << "Would you like to quit? Enter q for quit, and any other key to continue. " << endl;
+        cout << "Would you like to quit? Enter q for quit, and any other letter to continue. " << endl;
         cin >> quitProgram;
         if (quitProgram == 'q') {
             break;
@@ -37,6 +38,8 @@ int main() {
         else {
             cout << "Please enter a book's title or ISBN number: " << endl;
             cin >> input;
+            cout << "Retrieving book information..." << endl;
+            cout << endl;
             BookInformation book1(input);
             book1.Display();
         }
